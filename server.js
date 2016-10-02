9ef406cdbd4f6e4ebe4d9ef804d35b063bde93ca
Jason <jasonjones@salesforce.com>
2016-10-01 21:52:08 -0700

    Add user model and routes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+var express = require('express');
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var app = require('./app');
 
@@ -15,7 +17,48 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function () {
     console.log('We are connected to mongo container');
-})
+});
+
+var userSchema = new Schema({
+    name: { type: String},
+    email: {type: String},
+    admin: {type: Boolean, default: false},
+    dateCreated: {type: Date, default: Date.now()}
+});
+
+var User = mongoose.model('User', userSchema);
+
+var userRouter = express.Router();
+
+userRouter.get('/users', function (req, res) {
+    User.find({}, function (err, users) {
+        if (err) {
+            console.log(err);
+        }
+        if (users) {
+            console.log("returning all users...");
+            res.json(users);
+        } else {
+            res.json({success: false, msg: 'No users in database'});
+        }
+    });
+});
+
+userRouter.get('/user/:id', function (req, res) {
+    User.findById(req.params.id, function (err, user) {
+        if (err) {
+            console.log(err);
+        }
+        if (user) {
+            console.log("returning user...");
+            res.json(user);
+        } else {
+            res.json({success: false, msg: 'User not found'});
+        }
+    });
+});
+
+app.use('/api', userRouter);
 
 app.get('/', function (req, res) {
     res.json({msg: 'Hello world from Docker'});
