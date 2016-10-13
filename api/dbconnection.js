c7cda50dcdaa1adb906e6d502c0a4f8d8c1de0bd
Jason <jasonjones@salesforce.com>
2016-10-13 11:17:21 -0700

    Refactor db connection code to own file

---
+++ api/dbconnection.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+module.exports = function (config) {
+
+    mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
+
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error'));
+    db.once('open', function () {
+        console.log('We are connected to mongo container');
+    });
+
+}
--- api/server.js
+++ api/server.js
@@ -1,15 +1,9 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var config = require('./config');
 
 var app = require('./app');
 
-mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function () {
-    console.log('We are connected to mongo container');
-});
+require('./dbconnection')(config);
 
 var apiRouter = express.Router();
 require('./routes/dbutil')(apiRouter);
