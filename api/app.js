bf6d2f0a1d7f1bd37d6fa98dec386937362952a2
Jason <jasonjones@salesforce.com>
2016-10-14 21:25:32 -0700

    Add body-parser and morgan to project

--- api/app.js
+++ api/app.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
 
 module.exports = app;
