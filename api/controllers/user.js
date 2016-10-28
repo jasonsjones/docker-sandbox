0c76e988ba6fbb285b3adf4e5ad4a61d3e084b29
Jason <jasonjones@salesforce.com>
2016-10-27 21:13:42 -0700

    Add comments to user controller

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,8 @@
 var User = require('../models/user');
 
+// middleware controller function to find a user by their id
+// and then attach the user to the req object, this will make it
+// available to all routes on the /user/:id route
 exports.findUserById = function (req, res, next) {
     User.findById(req.params.id, function (err, user) {
         if (err) {
@@ -30,6 +33,8 @@ exports.getAllUsers = function (req, res) {
 };
 
 exports.addUser = function (req, res) {
+    // TODO: need to check if there is already a user created with the
+    // username provided
     var newUser = new User(req.body);
 
     newUser.save(function (err, user) {
