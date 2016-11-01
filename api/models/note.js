b8f0da496e0ae7cbac6240b5103490dffd8ff74d
Jason <jasonjones@salesforce.com>
2016-11-01 13:47:01 -0700

    Add timestamps to todo and note models

--- api/models/note.js
+++ api/models/note.js
@@ -1,9 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var noteSchema = new Schema({
-    content: {type: String},
-    parentId: {type: String}
-});
+var noteSchema = new Schema(
+    {
+        content: {type: String},
+        parentId: {type: String}
+    },
+    {
+        timestamps: {
+            createdAt: 'createdDate',
+            updatedAt: 'modifiedDate'
+        }
+    });
 
 module.exports = mongoose.model('Note', noteSchema);
--- api/models/todo.js
+++ api/models/todo.js
@@ -1,14 +1,21 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var todoSchema = new Schema({
-    item: {type: String},
-    completed: {type: Boolean, default: false},
-    createdDate: {type: Date, default: Date.now()},
-    modifiedDate: {type: Date, default: null},
-    dueDate: {type: Date, default: null},
-    note: {type: Schema.ObjectId, ref: 'Note'},
-    createdBy: {type: Schema.ObjectId, ref: 'User'}
-});
+var todoSchema = new Schema(
+    {
+        item: {type: String},
+        completed: {type: Boolean, default: false},
+        dueDate: {type: Date, default: null},
+        note: {type: Schema.ObjectId, ref: 'Note'},
+        createdBy: {type: Schema.ObjectId, ref: 'User'}
+    },
+    {
+        timestamps: {
+            createdAt: 'createdDate',
+            updatedAt: 'modifiedDate'
+        }
+    });
+
+todoSchema.set('toJSON', {virtuals: true});
 
 module.exports = mongoose.model('Todo', todoSchema);
