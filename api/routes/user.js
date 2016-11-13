74937532688f060cb947520163592c501e313e23
Jason <jasonjones@salesforce.com>
2016-11-12 22:27:00 -0800

    Add GET /users/hashDefaultPasswords endpoint -- wip

--- api/routes/user.js
+++ api/routes/user.js
@@ -22,7 +22,6 @@ module.exports = function (apiRouter) {
         .delete(userController.deleteUser);
 
     apiRouter.post('/login', function (req, res) {
-        console.log(req.body);
         User.find({'local.username': req.body.username}, function (err, user) {
             if (err) {
                 res.status(500).send(err);
@@ -47,4 +46,41 @@ module.exports = function (apiRouter) {
             }
         });
     });
+
+    apiRouter.get('/users/hashDefaultPasswords', function (req, res) {
+        User.find({}, function (err, users) {
+            if (err) {
+                res.status(500).send(err);
+            }
+            if (users) {
+                console.log('hash the passwords...');
+                hashPasswords(users)
+                    .then(function (result) {
+                        res.json(result);
+                    });
+            } else {
+                res.json({success: false, msg: 'No users in database'});
+            }
+        });
+    });
+
+    function hashPasswords(users) {
+        return new Promise(function (resolve, reject) {
+            users.forEach(function (user, idx, arr) {
+                console.log(user.name.full);
+                console.log(user.local.password);
+                console.log(user.hashDefaultPassword());
+                if (idx === arr.length - 1) {
+                    resolve({success: true,
+                             msg: 'passwords hashed with a promise'
+                    });
+                }
+
+                if (idx === arr.length) {
+                    reject({success: false});
+                }
+            });
+
+        });
+    }
 };
