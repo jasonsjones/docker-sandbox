6d2a5cf816983efc434eb278d6c595b1060acf6e
Jason <jasonjones@salesforce.com>
2016-11-13 22:07:41 -0800

    Add skeleton for testing w/ mocha & chai

---
+++ tests/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('The mocha test framework', function () {
+    it('is working', function () {
+        expect(true).to.be.true;
+    });
+});
