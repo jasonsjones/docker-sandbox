eee82e9e6fa01a351d709a8df65d6da215f275e5
Jason <jasonjones@salesforce.com>
2016-11-18 14:01:50 -0800

    Add test-setup file

--- tests/test.js
+++ tests/test.js
@@ -3,6 +3,7 @@
 var chai = require('chai');
 var expect = chai.expect;
 
+console.log('NODE_ENV: ' + process.env.NODE_ENV);
 describe('The mocha test framework', function () {
     it('is working', function () {
         expect(true).to.be.true;
