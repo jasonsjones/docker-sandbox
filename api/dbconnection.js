75268fdf2a27258fae04214f5eeecbe216c795be
Jason <jasonjones@salesforce.com>
2016-11-22 08:03:55 -0800

    Change notification string for db connection

--- api/dbconnection.js
+++ api/dbconnection.js
@@ -6,8 +6,8 @@ module.exports = function (config) {
 
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error'));
+    
     db.once('open', function () {
-        console.log('We are connected to mongo container');
+        console.log('connected to mongo container...');
     });
-
 };
