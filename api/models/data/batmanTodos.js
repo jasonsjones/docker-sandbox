c2c7e582243882f14ba99aab2562d64759710bb9
Jason <jasonjones@salesforce.com>
2016-11-25 21:36:08 -0800

    Fix linting issue

--- api/models/data/batmanTodos.js
+++ api/models/data/batmanTodos.js
@@ -22,4 +22,4 @@ module.exports = function (user) {
         }
     ];
     return batmanTodos;
-}
+};
--- api/models/data/supermanTodos.js
+++ api/models/data/supermanTodos.js
@@ -20,4 +20,4 @@ module.exports = function (user) {
         }
     ];
     return supermanTodos;
-}
+};
