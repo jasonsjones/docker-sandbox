287414f41a6da46194fdf96c182db213d64066b5
Jason <jasonjones@salesforce.com>
2016-12-02 09:40:02 -0800

    Complete TOH Step 3, Master/Detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,34 +6,38 @@ export class Hero {
 }
 
 const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
 ];
 
 @Component({
     selector: 'my-app',
-    template:`
+    template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-        <li *ngFor="let hero of heroes">
+        <li *ngFor="let hero of heroes" 
+          [class.selected]="hero === selectedHero"
+          (click)="onSelect(hero)">
             <!-- each hero goes here -->
             <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
     </ul>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
+    <div *ngIf="selectedHero">
+        <h2>{{selectedHero.name}} details!</h2>
+        <div><label>id: </label>{{selectedHero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="selectedHero.name" placeholder="name">
+        </div>
     </div>
     `,
     styles: [`
@@ -88,7 +92,12 @@ const HEROES: Hero[] = [
 
 })
 
-export class AppComponent { 
+export class AppComponent {
     title = 'Tour of Heroes';
+    selectedHero: Hero;
     heroes = HEROES;
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
