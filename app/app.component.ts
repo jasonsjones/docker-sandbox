60bc3abda017f93d72060a3412341351a0da7362
Jason <jasonjones@salesforce.com>
2016-12-08 20:38:58 -0800

    Add skeleton for dashboard cmp -- wip

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+    <a routerLink="/dashboard">Heroes</a>
     <a routerLink="/heroes">Heroes</a>
     <router-oulet></router-oulet>
   `
