239e7b6f71f141d0aeef87c0cf96d9fb3aa9f75e
Jason <jasonjones@salesforce.com>
2016-12-22 13:04:57 -0800

    Add a hero

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -36,6 +36,14 @@ export class HeroService {
             .catch(this.handleError);
     }
 
+    create(name: String): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+            .toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
+
     private handleError(error: any): Promise<any> {
         console.error('An error occured ', error); // for demo purposes only
         return Promise.reject(error.message || error);
